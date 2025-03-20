ee7abf828607944ed914660b2e95ff91332bd8c0
Minf-afk <msantana.inf@gmail.com>
2025-03-20 17:25:06 -0300

    first commit

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -3,7 +3,25 @@ import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
 /** @see: https://docs.keycloakify.dev/features/i18n */
-const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+const { useI18n, ofTypeI18n } = i18nBuilder
+    .withThemeName<ThemeName>()
+    .withCustomTranslations({
+        "pt": {
+            "usernameOrEmail": "",
+            "loginTitle": "Acesse sua conta", 
+            "doLogIn": "Entrar",
+            "username": "Usuário",
+            "password": "Senha",
+            "rememberMe": "Lembrar de mim",
+            "doRegister": "Cadastre-se Agora",
+            "noAccount": "Ainda não tem uma conta?",
+            "loginAccountTitle": "Minha Conta",
+            "identity-provider-login-label": "Ou entre com uma rede social",
+            "doForgotPassword": "Esqueceu a senha?",
+            "placeholderEmail": "SEU@EMAIL.COM"
+        }
+    })
+    .build();
 
 type I18n = typeof ofTypeI18n;
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,15 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { KcPage } from "./kc.gen";
 
-// The following block can be uncommented to test a specific page with `yarn dev`
-// Don't forget to comment back or your bundle size will increase
-/*
+import "./global.css";
 import { getKcContextMock } from "./login/KcPageStory";
 
 if (import.meta.env.DEV) {
     window.kcContext = getKcContextMock({
-        pageId: "register.ftl",
+        pageId: "login.ftl",
         overrides: {}
     });
 }
-*/
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
